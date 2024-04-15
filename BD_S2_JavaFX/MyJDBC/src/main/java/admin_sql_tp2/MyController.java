5b9f96e8a613b295ee6ce695331a5613ef67d71e
Yoshi <andreolienzo@gmail.com>
2024-04-15 14:35:45 +0200

    UPDATE

---
+++ BD_S2_JavaFX/MyJDBC/src/main/java/admin_sql_tp2/MyController.java
@@ -0,0 +1,60 @@
+package admin_sql_tp2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
+public class MyController {
+
+    @FXML
+    private Button myConnexionButton;
+
+    @FXML
+    private Label myConnexionLabel;
+
+    @FXML
+    private TextField myIdentityTextField;
+
+    @FXML
+    private TextField myPasswordTextField;
+
+    @FXML
+    private Button myQueryButton;
+
+    @FXML
+    private TextField myQueryTextField;
+
+    @FXML
+    private Label myResultLabel;
+
+    private MyJDBC myJDBC=new MyJDBC("com.microsoft.sqlserver.jdbc.SQLServerDriver","jdbc:sqlserver://172.30.4.243\\LHAMON;databaseName=testaccountDB");
+
+    private boolean connected=false;
+
+    @FXML
+    void connexionPressEvent(ActionEvent event)
+            throws ClassNotFoundException, SQLException {
+            if(!connected){
+                myJDBC.connect(myIdentityTextField.getText(),
+                        myPasswordTextField.getText());
+                myConnexionLabel.setText("Connecté");
+                connected=true;
+            }
+            else{
+                myJDBC.disconnect();
+                myConnexionLabel.setText("Pas connecté");
+                connected=false;
+            }
+    }
+
+    @FXML
+    void executionPressEvent(ActionEvent event)
+            throws SQLException {
+        String result=myJDBC.executeReadQuery(myQueryTextField.getText());
+        myResultLabel.setText(result);
+    }
+
+}
