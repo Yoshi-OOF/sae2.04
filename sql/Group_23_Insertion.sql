60a30cd187daca4115e5a70a10a70f57aa060343
Yoshi <andreolienzo@gmail.com>
2024-04-16 15:33:04 +0200

    Update

--- sql/Group_23_Insertion.sql
+++ sql/Group_23_Insertion.sql
@@ -1,6 +1,12 @@
-INSERT INTO etudiant VALUES(1, 'Bertrand', 'Emma', '18 Rue de Troy', 'emma.bertrand@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
-INSERT INTO etudiant VALUES(2, 'Bertrand', 'Léo', '18 Rue de Troy', 'leo.bertrand@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
-INSERT INTO etudiant VALUES(3, 'Blanc', 'Jade', '18 Rue de Troy', 'jade.blanc@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
-INSERT INTO etudiant VALUES(3, 'Blanc', 'Lola', '18 Rue de Troy', 'lola.blanc@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
-INSERT INTO etudiant VALUES(3, 'Blanc', 'Nathan', '18 Rue de Troy', 'nathan.blanc@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
-INSERT INTO etudiant VALUES(3, 'Blanc', 'Romane', '18 Rue de Troy', 'romane.blanc@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
+INSERT INTO etudiant VALUES(1, 'Bertrand', 'Emma', '18 Rue de Troy', 'emma.bertrand.etu@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
+INSERT INTO etudiant VALUES(2, 'Bertrand', 'Léo', '18 Rue de Troy', 'leo.bertrand.etu@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
+INSERT INTO etudiant VALUES(3, 'Blanc', 'Jade', '18 Rue de Troy', 'jade.blanc.etu@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
+INSERT INTO etudiant VALUES(3, 'Blanc', 'Lola', '18 Rue de Troy', 'lola.blanc.etu@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
+INSERT INTO etudiant VALUES(3, 'Blanc', 'Nathan', '18 Rue de Troy', 'nathan.blanc.etu@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
+INSERT INTO etudiant VALUES(3, 'Blanc', 'Romane', '18 Rue de Troy', 'romane.blanc.etu@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
+
+INSERT INTO enseignant VALUES(1, 'Vieillard', 'Nathalie', 'nathalie.vieillard@univ-lemans.fr', TRUE);
+INSERT INTO enseignant VALUES(1, 'Ernet', 'Bruno', 'bruno.ernet@univ-lemans.fr', TRUE);
+INSERT INTO enseignant VALUES(1, 'Laforcade', 'Pierre', 'pierre.laforcade@univ-lemans.fr', TRUE);
+INSERT INTO enseignant VALUES(1, 'Barré', 'Vincent', 'vincent.barre@univ-lemans.fr', TRUE);
+INSERT INTO enseignant VALUES(1, 'Marfisi', 'Iza', 'iza.marfisi@univ-lemans.fr', TRUE);
