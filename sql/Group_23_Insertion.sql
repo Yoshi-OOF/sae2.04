fd384ff755ea2c32d73e6b1f9ad7f74bcea8d16a
Maxime <72976426+Maximaximus72@users.noreply.github.com>
2024-04-18 14:00:44 +0200

    Update Group_23_Insertion.sql

--- sql/Group_23_Insertion.sql
+++ sql/Group_23_Insertion.sql
@@ -37,8 +37,8 @@ INSERT INTO STAGE VALUES(
     1,'Développement d’une application d’acquisition et de gestion de données ', 'Il s'agit de développer une base de données pour la fabrication des bandes platrées, récupérer
     les informations machine à intégrer dans la base, enregistrer les données et pouvoir les suivre
     de la fabrication des masses à leur conditionnement', '10/04/2017', '31/08/2017', 763, 
-    'Ordinateur portable, Accès VPN', 'Visual Studio Code, Docker', 'Windows 10, Serveur distant', 'JavaScript, Python', 'Présentielle', 'Distance', 
-    12, 13, 17, 11, 14, 16, 12, 15, 'Technique', 1, 123456789, 1);
+    'Ordinateur portable, Accès VPN', 'MySQL serveur', 'Windows 10, Serveur distant', 'SQL', 'envoyé', 'rempli', 
+    12, 13, 17, 11, 14, 16, 12, 15, 'Base de données', 1, 123456789, 1);
 
 INSERT INTO STAGE VALUES(
     2,'Consultant JAVA SPRING HADOOP', 'Intégré(e) au sein d’une équipe projet gérant les applications en lien avec les solution de gestion de campagnes 
@@ -46,7 +46,12 @@ INSERT INTO STAGE VALUES(
     intervenir sur les phases de conception technique, réalisation et validation de l’application. Les développements réalisés s’appuieront 
     principalement sur les technologies suivantes : langages SQL, JavaScript, Java, Spring, paramétrage du progiciel IBM Marketing, HSQL (Hadoop), Oracle ODI (ETL)',
     '04/05/2020', '31/07/2020', 782, 
-    'Ordinateur portable, Accès VPN', 'Visual Studio Code, Docker', 'Windows 10, Serveur distant', 'JavaScript, Python', 'Présentielle', 'Distance', 
-    12, 13, 17, 11, 14, 16, 12, 15, 'Technique', 2, 987654321, 2);
+    'Ordinateur portable', 'Eclipse, MySQL serveur', 'Windows 10, Serveur distant', 'SQL, JavaScript, Python', 'non envoyé', 'rempli', 
+    8, 11, 10, 7, 14, 11, 12, 12, 'Développement', 2, 987654321, 2);
 
+INSERT INTO STAGE VALUES(
+    3,'Mobile Development Internship', 'Take part in the day-day activities of the mobile development team Assist senior members with the development of 
+    new app features, Debugging testing and making app changes Work, experience with both iOS and Androïd', '09/04/2018', '22/06/2018', 867, 
+    'Ordinateur portable', 'Visual Studio Code, Docker', 'Windows 11, Serveur distant', 'JavaScript, Python', 'non envoyé', 'envoyé', 
+    18, 15, 16, 18, 14, 16, 17, 19, 'Mobile', 3, 112233445, 3);
 
