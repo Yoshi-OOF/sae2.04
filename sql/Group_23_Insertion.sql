e71b6df49cbfb243f522af8676bd5e17821c06de
Maxime <72976426+Maximaximus72@users.noreply.github.com>
2024-04-18 10:33:32 +0200

    Update Group_23_Insertion.sql

--- sql/Group_23_Insertion.sql
+++ sql/Group_23_Insertion.sql
@@ -1,13 +1,13 @@
-INSERT INTO etudiant VALUES(1, 'Bertrand', 'Emma', '18 Rue de Troy', 'emma.bertrand.etu@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
-INSERT INTO etudiant VALUES(2, 'Bertrand', 'Léo', '18 Rue de Troy', 'leo.bertrand.etu@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
-INSERT INTO etudiant VALUES(3, 'Blanc', 'Jade', '18 Rue de Troy', 'jade.blanc.etu@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
-INSERT INTO etudiant VALUES(4, 'Blanc', 'Lola', '18 Rue de Troy', 'lola.blanc.etu@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
-INSERT INTO etudiant VALUES(5, 'Blanc', 'Nathan', '18 Rue de Troy', 'nathan.blanc.etu@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
-INSERT INTO etudiant VALUES(6, 'Blanc', 'Romane', '18 Rue de Troy', 'romane.blanc.etu@univ-lemans.fr', '0783373684', TRUE, 'tongrog/lol', TRUE, 1);
+INSERT INTO etudiant VALUES(1, 'Dupond', 'Emma', '38 rue de Neveu', 'emma.Dupond.etu@univ-lemans.fr', '0628657135', TRUE, 'C:\Users\emma\Documents\attestation', FALSE, 1);
+INSERT INTO etudiant VALUES(2, 'Bertrand', 'Léo', '85 place de Boulanger', 'leo.bertrand.etu@univ-lemans.fr', '0756985412', TRUE, 'D:\Users\leo\Documents\attestation', TRUE, 2);
+INSERT INTO etudiant VALUES(3, 'Petit', 'Jade', '79 rue Lacroix', 'jade.Petit.etu@univ-lemans.fr', '0789654123', TRUE, 'C:\Users\jade\Documents\attestation', FALSE, 3);
+INSERT INTO etudiant VALUES(4, 'Leroy', 'Lola', '60 rue René Barthelemy', 'lola.Leroy.etu@univ-lemans.fr', '0625369512', TRUE, 'E:\Users\lola\Documents\attestation', TRUE, 4);
+INSERT INTO etudiant VALUES(5, 'Simon', 'Nathan', '6 impasse de Hernandez', 'nathan.Simon.etu@univ-lemans.fr', '0789542475', TRUE, 'C:\Users\nathan\Documents\attestation', FALSE, 5);
+INSERT INTO etudiant VALUES(6, 'Fournier', 'Romane', '18 Rue de Troy', 'romane.Fournier.etu@univ-lemans.fr', '0639821734', TRUE, 'D:\Users\romane\Documents\attestation', TRUE, 6);
 
-INSERT INTO enseignant VALUES(1, 'Vieillard', 'Nathalie', 'nathalie.vieillard@univ-lemans.fr', TRUE);
-INSERT INTO enseignant VALUES(2, 'Ernet', 'Bruno', 'bruno.ernet@univ-lemans.fr', TRUE);
-INSERT INTO enseignant VALUES(3, 'Laforcade', 'Pierre', 'pierre.laforcade@univ-lemans.fr', TRUE);
-INSERT INTO enseignant VALUES(4, 'Barré', 'Vincent', 'vincent.barre@univ-lemans.fr', TRUE);
-INSERT INTO enseignant VALUES(5, 'Marfisi', 'Iza', 'iza.marfisi@univ-lemans.fr', TRUE);
+INSERT INTO enseignant VALUES(1, 'Vieillard', 'Nathalie', 'nathalie.vieillard@univ-lemans.fr', FALSE);
+INSERT INTO enseignant VALUES(2, 'Ernet', 'Bruno', 'bruno.ernet@univ-lemans.fr', FALSE);
+INSERT INTO enseignant VALUES(3, 'Laforcade', 'Pierre', 'pierre.laforcade@univ-lemans.fr', FALSE);
+INSERT INTO enseignant VALUES(4, 'Barré', 'Vincent', 'vincent.barre@univ-lemans.fr', FALSE);
+INSERT INTO enseignant VALUES(5, 'Marfisi', 'Iza', 'iza.marfisi@univ-lemans.fr', FALSE);
 INSERT INTO enseignant VALUES(6, 'Hamon', 'Ludovic', 'Hamon.Ludovic@univ-lemans.fr', TRUE);
