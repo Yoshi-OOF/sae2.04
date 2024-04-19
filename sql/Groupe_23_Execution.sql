17354b9c2878372ed64584c1e75ade0007872b85
Maxime <72976426+Maximaximus72@users.noreply.github.com>
2024-04-19 09:18:25 +0200

    Update Groupe_23_Execution.sql

--- sql/Groupe_23_Execution.sql
+++ sql/Groupe_23_Execution.sql
@@ -1,34 +1,50 @@
 -- Règle 17.1 : 
 
 -- Voir tous les étudiants
-USE groupe_23;
+USE Groupe_23;
 SELECT * FROM vue_secretaire_etudiant;
 
 -- Voir tous les stages
-USE groupe_23;
+USE Groupe_23;
 SELECT * FROM vue_secretaire_stage;
 
 -- Voir tous les maitre de stage
-USE groupe_23;
+USE Groupe_23;
 SELECT * FROM vue_secretaire_maitre_stage;
 
 -- Voir tous les superviseurs
-USE groupe_23;
+USE Groupe_23;
 SELECT * FROM vue_secretaire_superviseur;
 
 -- Voir tous les enseignants
-USE groupe_23;
+USE Groupe_23;
 SELECT * FROM vue_secretaire_enseignant;
 
 -- Modifier un étudiant
-USE groupe_23;
+USE Groupe_23;
 UPDATE ETUDIANT SET Chemin_attes_etu = 'C:\Users\Emma\Documents\attes' WHERE Mail_etu = 'emma.bertrand.etu@univ-lemans.fr';
 
 -- Modifier une entreprise
 USE Groupe_23;
 UPDATE ENTREPRISE SET Adresse_chef_ent = '123 Avenue des Champs Élysées' WHERE Mail_chef_ent = 'lucas.martel@ecodynamics.com';
-
-USE Groupe_23;
 UPDATE ENTREPRISE SET Nom_chef_ent = 'BARDOU', Prenom_chef_ent = 'Maxime', Adresse_chef_ent = '35 rue des Hortensias, 75002 Paris', Mail_chef_ent = 'maxime.bardou@sogeti.com'
 WHERE Adresse_chef_ent = 'damien.roux@findway.com' AND Nom_ent = 'VINCI Energies Systèmes d''Information';
+
+-- Modifier un maitre de stage
+USE Groupe_23;
+UPDATE MAITRE_STAGE SET Poste_maitreStage = 'CDD' WHERE Mail_maitreStage = 'thierry.couanon@bsnmedical.com';
+UPDATE MAITRE_STAGE SET Tel_maitreStage = '0748256739' WHERE Mail_maitreStage = 'pierre.tijou@shortways.com';
+
+--Modifier un superviseur 
+USE Groupe_23;
+UPDATE SUPERVISEUR SET Statut_superviseur = 'Contractuel' WHERE Mail_superviseur = 'lucie.martin@corp.com';
+UPDATE SUPERVISEUR SET Poste_superviseur = 'Consultant IT' WHERE Mail_superviseur = 'alexandre.richard@marketingcorp.com';
+
+-- Règle 17.2 : 
+-- voir les notes d'un étudiant
+USE Groupe_23;
+SELECT S.Id_stage, S.Titre_mission, S.Fond_rapport_note, S.Forme_rapport_note, S.Fond_soutenance_note, S.Forme_soutenance_note, S.Jury_soutenance_note, 
+S.Methode_ent_note, S.Travail_ent_note, S.Compor_ent_note FROM STAGE S
+JOIN ETUDIANT E ON S.Id_etu = E.Id_etu WHERE E.Mail_etu = 'leo.bertrand.etu@univ-lemans.fr';
+
 
