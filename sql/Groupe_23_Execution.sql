4f4ae8490ec497401a0abfcaaaf58037e908b300
Yoshi <andreolienzo@gmail.com>
2024-04-19 08:28:21 +0200

    aaa

--- sql/Groupe_23_Execution.sql
+++ sql/Groupe_23_Execution.sql
@@ -1,6 +1,15 @@
+-- Voir tous les étudiants
 SELECT * FROM vue_secretaire_etudiant;
+
+-- Voir tous les enseignants
 SELECT * FROM vue_secretaire_stage;
+
+-- Voir tous les enseignants
 SELECT * FROM vue_secretaire_maitre_stage;
+
+-- Voir tous les enseignants
 SELECT * FROM vue_secretaire_superviseur;
+
+-- Voir tous les enseignants
 SELECT * FROM vue_secretaire_enseignant;
 
