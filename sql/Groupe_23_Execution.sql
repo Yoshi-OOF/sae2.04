787adca0328fde1472e8fdeee2064c49647d7855
Yoshi <andreolienzo@gmail.com>
2024-04-19 08:26:23 +0200

    aaa

---
+++ sql/Groupe_23_Execution.sql
@@ -0,0 +1,6 @@
+SELECT * FROM vue_secretaire_etudiant;
+SELECT * FROM vue_secretaire_stage;
+SELECT * FROM vue_secretaire_maitre_stage;
+SELECT * FROM vue_secretaire_superviseur;
+SELECT * FROM vue_secretaire_enseignant;
+
