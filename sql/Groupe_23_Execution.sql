01c4c48313c7dd37b76e12cbc976773e56146e4f
Maxime <72976426+Maximaximus72@users.noreply.github.com>
2024-04-19 08:48:19 +0200

    Update Groupe_23_Execution.sql

--- sql/Groupe_23_Execution.sql
+++ sql/Groupe_23_Execution.sql
@@ -1,15 +1,25 @@
+-- Règle 17.1 : 
+
 -- Voir tous les étudiants
+USE groupe_23;
 SELECT * FROM vue_secretaire_etudiant;
 
 -- Voir tous les stages
+USE groupe_23;
 SELECT * FROM vue_secretaire_stage;
 
 -- Voir tous les maitre de stage
+USE groupe_23;
 SELECT * FROM vue_secretaire_maitre_stage;
 
 -- Voir tous les superviseurs
+USE groupe_23;
 SELECT * FROM vue_secretaire_superviseur;
 
 -- Voir tous les enseignants
+USE groupe_23;
 SELECT * FROM vue_secretaire_enseignant;
 
+-- Update l'étudiant n°1
+USE groupe_23;
+UPDATE ETUDIANT SET Chemin_attes_etu = 'C:\Users\Emma\Documents\attes' WHERE Mail_etu = 'emma.bertrand.etu@univ-lemans.fr';
