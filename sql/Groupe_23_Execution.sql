aa813bb068b4c4f539cfd988cab0b6b163230ff9
Maxime <72976426+Maximaximus72@users.noreply.github.com>
2024-04-19 08:55:24 +0200

    Update Groupe_23_Execution.sql

--- sql/Groupe_23_Execution.sql
+++ sql/Groupe_23_Execution.sql
@@ -23,3 +23,12 @@ SELECT * FROM vue_secretaire_enseignant;
 -- Modifier un étudiant
 USE groupe_23;
 UPDATE ETUDIANT SET Chemin_attes_etu = 'C:\Users\Emma\Documents\attes' WHERE Mail_etu = 'emma.bertrand.etu@univ-lemans.fr';
+
+-- Modifier une entreprise
+USE Groupe_23;
+UPDATE ENTREPRISE SET Adresse_chef_ent = '123 Avenue des Champs Élysées' WHERE Mail_chef_ent = 'lucas.martel@ecodynamics.com';
+
+USE Groupe_23;
+UPDATE ENTREPRISE SET Nom_chef_ent = 'BARDOU', Prenom_chef_ent = 'Maxime', Adresse_chef_ent = '35 rue des Hortensias, 75002 Paris', Mail_chef_ent = 'maxime.bardou@sogeti.com'
+WHERE Adresse_chef_ent = 'damien.roux@findway.com' AND Nom_ent = 'VINCI Energies Systèmes d''Information';
+
